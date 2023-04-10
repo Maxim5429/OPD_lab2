136b35f87e1ec772cf59d64cbd3aaf2c5ae94038
Maxim_Petrov <max@itproger.com>
2023-04-10 23:32:35 +0600

    first commit

---
+++ main.py
@@ -0,0 +1,4 @@
+from aiogram import Bot,Dispatcher,executor,types
+TOKEN_API="6049796693:AAGMP8YSgH9Npcmph1DdPujJDLkibuX_AMs" # авторизационный токен для получения телеграмм API
+bot= Bot(TOKEN_API)
+dp= Dispatcher(bot)
