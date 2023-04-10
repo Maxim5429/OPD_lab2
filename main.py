ee512fd854008713526db8f37edaba1294a4ade6
Maxim_Petrov <max@itproger.com>
2023-04-10 23:39:13 +0600

    second commit

--- main.py
+++ main.py
@@ -2,3 +2,10 @@ from aiogram import Bot,Dispatcher,executor,types
 TOKEN_API="6049796693:AAGMP8YSgH9Npcmph1DdPujJDLkibuX_AMs" # авторизационный токен для получения телеграмм API
 bot= Bot(TOKEN_API)
 dp= Dispatcher(bot)
+
+dp.message_handler()
+async def echo(message:types.Message):
+    await message.answer(text=message.text)
+
+if __name__=='__main__':
+    executor.start_polling(dp)
